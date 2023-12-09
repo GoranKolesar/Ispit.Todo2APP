826bc33106e10595158811854d76a56647a6dafb
Goran Kolesar <gorankolesar@yahoo.com>
2023-12-10 00:28:15 +0100

    Add project files.

---
+++ Ispit.Todo_GoranKolesar/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Ispit.Todo_GoranKolesar.Models
+{
+    public class Category
+    {
+        public string CategoryId { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ Ispit.Todo_GoranKolesar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ispit.Todo_GoranKolesar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
