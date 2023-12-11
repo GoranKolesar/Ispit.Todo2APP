2d6dd0b2378ef1e991c495336809b43ba56f544a
Goran Kolesar <gorankolesar@yahoo.com>
2023-12-11 19:35:43 +0100

    Dotjerivanje

--- Ispit.Todo_GoranKolesar/Program.cs
+++ Ispit.Todo_GoranKolesar/Program.cs
@@ -17,7 +17,7 @@ namespace Ispit.Todo_GoranKolesar
 
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ToDoContext>();
             builder.Services.AddControllersWithViews();
 
